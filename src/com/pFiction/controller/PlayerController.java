bad311dfe596b20a52d6054f8bed3ee55c7bc012
Danzai-Senpai <65458407+Danzai-Senpai@users.noreply.github.com>
2021-10-11 10:25:37 -0300

    Finalização de adição de sprites
    Erro de JavaNullLangException com SpriteLibrary retornando valor null. Não entendo o porque disto está ocorrendo, uma analise maior é necessaria.

--- src/com/pFiction/controller/PlayerController.java
+++ src/com/pFiction/controller/PlayerController.java
@@ -14,49 +14,41 @@ public class PlayerController implements Controller {
 
 	@Override
 	public boolean isRequestingLeft() {
-		// TODO Auto-generated method stub
 		return input.isPressed(KeyEvent.VK_LEFT);
 	}
 
 	@Override
 	public boolean isRequestingRight() {
-		// TODO Auto-generated method stub
 		return input.isPressed(KeyEvent.VK_RIGHT);
 	}
 
 	@Override
 	public boolean isRequestingUp() {
-		// TODO Auto-generated method stub
 		return input.isPressed(KeyEvent.VK_UP);
 	}
 
 	@Override
 	public boolean isRequestingDown() {
-		// TODO Auto-generated method stub
 		return input.isPressed(KeyEvent.VK_DOWN);
 	}
 
 	@Override
 	public boolean isRequestingW() {
-		// TODO Auto-generated method stub
 		return input.isPressed(KeyEvent.VK_W);
 	}
 
 	@Override
 	public boolean isRequestingA() {
-		// TODO Auto-generated method stub
 		return input.isPressed(KeyEvent.VK_A);
 	}
 
 	@Override
 	public boolean isRequestingS() {
-		// TODO Auto-generated method stub
 		return input.isPressed(KeyEvent.VK_S);	
 		}
 
 	@Override
 	public boolean isRequestingD() {
-		// TODO Auto-generated method stub
 		return input.isPressed(KeyEvent.VK_D);
 		}
 
--- src/com/pFiction/entity/MovingEntity.java
+++ src/com/pFiction/entity/MovingEntity.java
@@ -2,17 +2,23 @@ package com.pFiction.entity;
 
 import com.pFiction.controller.Controller;
 import com.pFiction.core.Movement;
+import com.pFiction.gfx.AnimationManager;
+import com.pFiction.gfx.SpriteLibrary;
+
+import java.awt.*;
 
 public abstract class MovingEntity extends GameObject {
 	
 	private Controller controller;
 	private Movement movement;
+	private AnimationManager animationManager;
 	
-	public MovingEntity(Controller controller) {
+	public MovingEntity(Controller controller, SpriteLibrary spriteLibrary) {
 		
 		super();
 		this.controller = controller;
 		this.movement = new Movement(2);
+		animationManager = new AnimationManager(spriteLibrary.getUnit("henry"));
 		
 	}
 	
@@ -20,7 +26,13 @@ public abstract class MovingEntity extends GameObject {
 		
 		movement.update(controller);
 		position.apply(movement);
+		animationManager.update();
 		
+	}
+
+	@Override
+	public Image getSprite() {
+		return animationManager.getSprite();
 	}
 
 }
